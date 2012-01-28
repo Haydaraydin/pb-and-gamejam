fb28069195095cef18be0354714fed4180a3c783
plalonde@gmail.com <plalonde@gmail.com@9b99789d-59c5-4ca9-b294-fc7b6f66b05d>
2012-01-28 17:04:39 +0000

    Adding infinite tunnels with cleanup of tunnels. Also setting camera far clip plane to 5.
    Reviewed by Francis.

---
+++ trunk/Assets/Scripts/TunnelSpawnerScript.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+public class TunnelSpawnerScript : MonoBehaviour {
+	
+	public GameObject tunnelPrefab;
+	public Transform character;
+	
+	public float segmentLength = 5.0f;
+	
+	private float lastSpawnedSegment;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Vector3 posBack = new Vector3(
+								character.position.x,
+		                    	character.position.y,
+		                    	character.position.z - segmentLength);
+		
+		Vector3 posMiddle = character.position;
+		
+		Vector3 posFront = new Vector3(
+								character.position.x,
+		                    	character.position.y,
+		                    	character.position.z + segmentLength);
+		
+		SpawnTunnel(posBack);
+		SpawnTunnel(posMiddle);
+		SpawnTunnel(posFront);
+		
+		lastSpawnedSegment = posFront.z;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(character.position.z > lastSpawnedSegment)
+		{
+			Vector3 spawnPos = new Vector3(
+								character.position.x,
+		                    	character.position.y,
+		                    	lastSpawnedSegment + segmentLength);
+			                               
+			SpawnTunnel(spawnPos);
+			
+			lastSpawnedSegment = spawnPos.z;
+		}
+			            
+	}
+	
+	void SpawnTunnel (Vector3 position)
+	{	
+		GameObject newTunnel;
+		newTunnel = Instantiate(tunnelPrefab, position, Quaternion.identity) as GameObject;
+		TunnelCleanUp cleanUp = newTunnel.GetComponent<TunnelCleanUp>();
+		cleanUp.SetCharacter(character);
+	}
+}
