b017c4741b18471d5b41b574cc9d9ad65491b081
fpetrin@gmail.com <fpetrin@gmail.com@9b99789d-59c5-4ca9-b294-fc7b6f66b05d>
2012-01-28 20:26:49 +0000

    Character collision with obstacles and Speed Power Up.
    Buddied by pat.

--- trunk/Assets/Scripts/PathFollowing.cs
+++ trunk/Assets/Scripts/PathFollowing.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
 using System.Collections;
 
-public class PathFollowing : MonoBehaviour {
+public class PathFollowing : MonoBehaviour 
+{
 
 	public Vector3 initialPosition;
-	public float moveSpeed = 1.0f;
+	public float defaultMoveSpeed = 1.0f;
+	private float moveSpeed;
 	public bool loop = false;
 	public float maxDistance = 5.0f;
 	
 	public GameObject tunnelSpawner;
 	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		transform.position = initialPosition;
+		moveSpeed = defaultMoveSpeed;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		Vector3 newPos = transform.position;
 		newPos += new Vector3(0, 0, moveSpeed*Time.deltaTime);
 		
@@ -36,5 +41,15 @@ public class PathFollowing : MonoBehaviour {
 	void OnGUI()
 	{
 		GUILayout.Label("Distance: " + transform.position.z);
+	}
+	
+	public void SetMoveSpeed(float newSpeed)
+	{
+		moveSpeed = newSpeed;
+	}
+	
+	public float GetMoveSpeed()
+	{
+		return moveSpeed;
 	}
 }
