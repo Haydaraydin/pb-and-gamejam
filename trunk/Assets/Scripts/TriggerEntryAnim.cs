5f3c8a884623d07c4571a763d222ed5c3c39341e
plalonde@gmail.com <plalonde@gmail.com@9b99789d-59c5-4ca9-b294-fc7b6f66b05d>
2012-01-29 02:55:49 +0000

    Adding intro animation activated using distance from camera.
    Reviewed by Francis.

---
+++ trunk/Assets/Scripts/TriggerEntryAnim.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class TriggerEntryAnim : MonoBehaviour 
+{
+	public GameObject objectToPlayOn;
+	public string entryClip;
+	public float distanceFromCamera = 5.0f;
+	
+	private Camera mainCam;
+	private bool played;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		mainCam = (Camera)FindObjectOfType(typeof(Camera));
+		
+		played = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 camPos = mainCam.transform.position;
+		
+		if(Mathf.Abs(camPos.z - transform.position.z) < distanceFromCamera && !played 
+		   && objectToPlayOn != null && objectToPlayOn.animation != null)
+		{
+			objectToPlayOn.animation.Play(entryClip);
+			played = true;
+		}
+	}
+}
