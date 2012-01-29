132c144dca6a54a0439b24e361f464025d0617d2
fpetrin <fpetrin@gmail.com>
2012-01-29 19:13:01 +0000

    Final checkin.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class MainMenu : MonoBehaviour 
 {
     public GUIStyle invisibleButton;
+	public Texture menuTexture;
+	public GUIStyle style;
+	public GUIStyle titleStyle;
 	
 	// Use this for initialization
 	void Start () 
@@ -20,28 +23,38 @@ public class MainMenu : MonoBehaviour
 	
 	void OnGUI()
 	{
-        if (GUI.Button(new Rect(0, 300, 400, 200), "", invisibleButton))
+		GUI.DrawTexture(new Rect(0,0,1280,800), menuTexture);
+		
+		GUI.Label(new Rect(50,50,900,500), "Constricted\n  Cheddar Chaser", titleStyle);
+		
+		GUI.Label(new Rect(172,305,300,100), "Start game (Progression)", style);
+		GUI.Label(new Rect(172,380,300,100), "Start game (Hard mode)", style);
+		
+		GUI.Label(new Rect(260,565,300,100), "A game by:", style);
+		GUI.Label(new Rect(260,615,700,200), 
+			"Lawrence Collier - Technical Artist\nAmanda Leduc - Sound Artist\nFrancis Petrin - Programmer\nPat Lalonde - Programmer", style);
+		
+        if (GUI.Button(new Rect(0, 275, 400, 100), "", invisibleButton))
         {
+			StaticGameProperties.GetInstance().SetLevelToStartAt(0);
+            Application.LoadLevel(1);
+        }
+		
+        if (GUI.Button(new Rect(0, 375, 400, 100), "", invisibleButton))
+        {
+			StaticGameProperties.GetInstance().SetLevelToStartAt(7);
             Application.LoadLevel(1);
         }
 	}
 }
 
-public class StaticGameProperties : MonoBehaviour
+public class StaticGameProperties
 {
 	
 	private static int levelToStartAt = 0;
 	
 	private static StaticGameProperties instance;
 	
-	void Start()
-	{
-		DontDestroyOnLoad(this);
-	}
-	
-	void Update()
-	{
-	}
 	
 	public static StaticGameProperties GetInstance()
 	{
